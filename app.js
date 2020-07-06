532fe83ae7d0319c42f7d0607dd4659779ce9c65
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-07-06 23:57:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const app = require("express")();
-const http = require("http").Server(app);
-
-app.get("/", (req, res) => {
+const app = require("express");
+const server = app();
+server.get("/", (req, res) => {
   res.sendfile("index.html");
 });
 
-http.listen(3000, () => {
+server.listen(3000, () => {
   console.log("start server on port: 3000");
 });
