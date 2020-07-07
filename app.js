c8db169edd6dfc0bd9b6770e32c49499a6197aa0
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-07-07 13:43:27 +0700

    Nodejs + Socket.io ตอนที่ 3 - Emit & Broadcast
     Nodejs + Socket.io ตอนที่ 3 - Emit & Broadcast

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const app = require("express");
 const server = app();
+const http = require("http").Server(server);
+const io = require("socket.io")(http);
+
 server.get("/", (req, res) => {
   res.sendfile("index.html");
 });
 
-server.listen(3000, () => {
-  console.log("start server on port: 3000");
+let clients = 0; // คอยนับจำนวนเครื่องที่เชื่อมต่อ
+io.on("connection", (socket) => {
+  clients++;
+  io.sockets.emit("broadcast", {
+    message: clients + " clients connected!",
+  });
+  socket.on("disconnect", () => {
+    clients--;
+    io.sockets.emit("broadcast", {
+      message: clients + " clients connected!",
+    });
+  });
+});
+
+http.listen(8080, () => {
+  console.log("start server on port: 8080");
 });
